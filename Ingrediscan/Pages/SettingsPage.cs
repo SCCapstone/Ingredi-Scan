50b7688015b7371c46f7fb14571e699b06208474
cbailie <cbailie@email.sc.edu>
2017-04-07 19:17:51 -0400

    Revert "Progress on settings search"
    This reverts commit 06adaeceed1895ea18a42966ab62515af2ca8600.

--- Ingrediscan/Pages/SettingsPage.cs
+++ Ingrediscan/Pages/SettingsPage.cs
@@ -6,76 +6,25 @@ using Xamarin.Forms;
 using Ingrediscan.Utilities;
 using Android.Widget;
 
-using XLabs.Forms.Controls;
-using System.Linq;
-
 namespace Ingrediscan
 {
 	public class SettingsPage : ContentPage
 	{
-        Label resultsLabel;
-        SearchBar searchBar;
-        public static List<string> _terms = new List<string> { "African", "Chinese", "Japanese", "Korean", "Vietnamese", "Thai", "Indian",
-            "British", "Irish", "French", "Italian", "Mexican", "Spanish", "Middle Eastern", "Jewish", "American", "Cajun",
-            "Southern", "Greek", "German", "Nordic", "Eastern European", "Caribbean", "Latin american", "Pescetarian", "Lacto Vegetarian",
-                 "Ovo Vegetarian", "Vegan", "Vegetarian", "Dairy", "Egg", "Gluten", "Peanut", "Sesame", "Seafood", "Shellfish"};
-        public SettingsPage ()
+		public SettingsPage ()
 		{
 
             Title = "Settings";
-            
-
-            resultsLabel = new Label
-            {
-                Text = "Result will appear here.",
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                FontSize = 25
-            };
-
-            searchBar = new SearchBar
-            {
-                Placeholder = "Enter search term",
-                SearchCommand = new Command(() => { resultsLabel.Text = "Result: " + searchBar.Text + " is what you asked for."; })
-            };
 
             var label = new Label
             {
-                Text = "Mark Items to Narrow Recipe Search.",
+                Text = "Mark Items to Narrow Search.",
                 FontSize = 20,
                 FontAttributes = FontAttributes.Bold,
                 HorizontalTextAlignment = TextAlignment.Center
             };
-
-            
-
-        var listView = new Xamarin.Forms.ListView();
-            listView.ItemsSource = _terms;
-
-            var popUpButton = new Xamarin.Forms.Button { Text = "Search Through Settings", TextColor = Color.White, BackgroundColor = Color.Blue };
-
-            PopupLayout _PopUpLayout = new PopupLayout();
-
-           
-
             StackLayout layout = new StackLayout();
 
-            searchBar.TextChanged += (sender, e) =>
-            {
-                listView.BeginRefresh();
-                if (string.IsNullOrWhiteSpace(e.NewTextValue.ToLower()))
-                    listView.ItemsSource = _terms;
-                else
-                    listView.ItemsSource = _terms.Where(i => i.ToLower().Contains(e.NewTextValue.ToLower()));
-                listView.EndRefresh();
-            };
-
-            layout.Children.Add(popUpButton);
-
-           
-
- 
-
-        Settings.LoadSettings(Globals.firebaseData.searchSettings);
+            Settings.LoadSettings(Globals.firebaseData.searchSettings);
 
 			ToolbarItems.Add (new ToolbarItem ("Clear Settings", "drawable/reset.png", async () => {
 				
@@ -92,9 +41,9 @@ namespace Ingrediscan
 
             Settings.SaveSettings();
 
-            
+           
 
-            var cuisineSwitches = Settings.populateSettings("cuisine");
+			var cuisineSwitches = Settings.populateSettings("cuisine");
 			var dietSwitches = Settings.populateSettings ("diet");
 			var intolSwitches = Settings.populateSettings ("intol");
 
@@ -119,46 +68,7 @@ namespace Ingrediscan
 
             layout.Children.Add(label);
             layout.Children.Add(Settings.tableView);
-
-            
-
-            listView.ItemTapped += (sender, e) =>
-            {
-               
-                
-                //e.Item.ToString() gets text from list, ex "Thai"
-                // was hoping this info could be used to go to tableview cell with same name, or toggle switch, but havn't been able to figure out how
-
-                if (_PopUpLayout.IsPopupActive)
-                {
-                    _PopUpLayout.DismissPopup();// Close the PopUp
-                }
-                layout.IsVisible = true;
-            };
-
-            var PopUp = new StackLayout
-            {
-                WidthRequest = 300,
-                HeightRequest = 400,
-                BackgroundColor = Color.White,
-                Orientation = StackOrientation.Vertical,
-                Children =
-                {
-                    searchBar,
-                    resultsLabel,
-                    listView,
-                }
-            };
-
-            popUpButton.Clicked += (sender, e) =>
-            {
-                layout.IsVisible = false;
-                _PopUpLayout.ShowPopup(PopUp);
-                searchBar.Focus(); // set Focus to SearchBar so that the user can start to type…
-            };
-
-            _PopUpLayout.Content = layout;
-            Content = _PopUpLayout;
+            Content = layout;
 		}
 	}
 }
