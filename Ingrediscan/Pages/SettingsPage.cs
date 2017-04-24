8783fda56b0c084efee2fcfeb1be5db7bd47954c
cbailie <cdbailie@charter.net>
2017-04-23 22:51:41 -0400

    Can search in settings page

--- Ingrediscan/Pages/SettingsPage.cs
+++ Ingrediscan/Pages/SettingsPage.cs
@@ -5,15 +5,36 @@ using Xamarin.Forms;
 
 using Ingrediscan.Utilities;
 using Android.Widget;
+using XLabs.Forms.Controls;
+using System.Linq;
 
 namespace Ingrediscan
 {
 	public class SettingsPage : ContentPage
 	{
-		public SettingsPage ()
+        Label resultsLabel;
+        SearchBar searchBar;
+        public static List<string> _terms = new List<string> { "African", "Chinese", "Japanese", "Korean", "Vietnamese", "Thai", "Indian",
+            "British", "Irish", "French", "Italian", "Mexican", "Spanish", "Middle Eastern", "Jewish", "American", "Cajun",
+            "Southern", "Greek", "German", "Nordic", "Eastern European", "Caribbean", "Latin american", "Pescetarian", "Lacto Vegetarian",
+                 "Ovo Vegetarian", "Vegan", "Vegetarian", "Dairy", "Egg", "Gluten", "Peanut", "Sesame", "Seafood", "Shellfish"};
+        public SettingsPage ()
 		{
 
             Title = "Settings";
+
+            resultsLabel = new Label
+            {
+                Text = "Result will appear here.",
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                FontSize = 25
+            };
+
+            searchBar = new SearchBar
+            {
+                Placeholder = "Enter search term",
+                SearchCommand = new Command(() => { resultsLabel.Text = "Result: " + searchBar.Text + " is what you asked for."; })
+            };
 
             var label = new Label
             {
@@ -22,7 +43,26 @@ namespace Ingrediscan
                 FontAttributes = FontAttributes.Bold,
                 HorizontalTextAlignment = TextAlignment.Center
             };
+
+            var listView = new Xamarin.Forms.ListView();
+            listView.ItemsSource = _terms;
+
+            var popUpButton = new Xamarin.Forms.Button { Text = "Search Through Settings", TextColor = Color.White, BackgroundColor = Color.Blue };
+
+            PopupLayout _PopUpLayout = new PopupLayout();
             StackLayout layout = new StackLayout();
+
+            searchBar.TextChanged += (sender, e) =>
+            {
+                listView.BeginRefresh();
+                if (string.IsNullOrWhiteSpace(e.NewTextValue.ToLower()))
+                    listView.ItemsSource = _terms;
+                else
+                    listView.ItemsSource = _terms.Where(i => i.ToLower().Contains(e.NewTextValue.ToLower()));
+                listView.EndRefresh();
+            };
+
+            layout.Children.Add(popUpButton);
 
             Settings.LoadSettings(Globals.firebaseData.searchSettings);
 
@@ -68,7 +108,64 @@ namespace Ingrediscan
 
             layout.Children.Add(label);
             layout.Children.Add(Settings.tableView);
-            Content = layout;
+
+            var PopUp = new StackLayout
+            {
+                WidthRequest = 300,
+                HeightRequest = 400,
+                BackgroundColor = Color.White,
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    searchBar,
+                    resultsLabel,
+                    listView,
+                }
+            };
+
+            Dictionary<string, SwitchCell> dictionary = new Dictionary<string, SwitchCell>();
+            {
+                foreach (var item in cuisineSwitches)
+                    dictionary.Add(item.Text,item);
+                foreach (var item in dietSwitches)
+                    dictionary.Add(item.Text, item);
+                foreach (var item in intolSwitches)
+                    dictionary.Add(item.Text, item);
+            };
+
+            popUpButton.Clicked += (sender, e) =>
+            {
+                layout.IsVisible = false;
+                _PopUpLayout.ShowPopup(PopUp);
+                searchBar.Focus(); // set Focus to SearchBar so that the user can start to type…
+            };
+
+            listView.ItemTapped += (sender, e) =>
+            {
+                if (dictionary.ContainsKey(e.Item.ToString()))
+                {
+                    SwitchCell value = new SwitchCell();
+                    dictionary.TryGetValue(e.Item.ToString(), out value);
+                    
+                    if (value.On == false)
+                        value.On = true;
+                    else if (value.On == true)
+                        value.On = false;
+                    Settings.SaveSettings();
+                }
+                //e.Item.ToString() gets text from list, ex "Thai"
+                // was hoping this info could be used to go to tableview cell with same name, or toggle switch, but havn't been able to figure out how
+
+                if (_PopUpLayout.IsPopupActive)
+                {
+                    _PopUpLayout.DismissPopup();// Close the PopUp
+                }
+                layout.IsVisible = true;
+            };
+
+            _PopUpLayout.Content = layout;
+            Content = _PopUpLayout;
+            
 		}
 	}
 }
